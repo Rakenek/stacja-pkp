2d1c311aaca12d5b3958c345cf31b26b1f3e1ea4
Rakenek <kamilotawski@gmail.com>
2018-05-09 21:10:24 +0200

    Projekt stacja pkp poczatek

---
+++ stacjapkpbackend/src/main/java/net/kamo/stacjapkpbackend/App.java
@@ -0,0 +1,13 @@
+package net.kamo.stacjapkpbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
